b977f480b1da0506002fcd3c042d267837a91de5
leirisyue <leirisyue@gmail.com>
2024-05-11 02:17:41 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # fed-yfinance-demo
+
+### start server
+```bash
+python -m pt_server -r 40
+```
+### Start client alice
+```bash
+python -m pt_client -s alice
+```
+
+### Start client alice
+```bash
+python -m pt_client -s bob
+```
+
+
+<div align="center">
+<img src="asset/workflow.png">
+</div>
--- client.py
+++ client.py
@@ -14,10 +14,10 @@ from dataset import prepare_dataset
 
 def get_data(split="all"):
     
-    # x, y = sklearn.datasets.load_iris(return_X_y=True)
+    x, y = sklearn.datasets.load_iris(return_X_y=True)
     # x, y = sklearn.datasets.load_digits(return_X_y=True)
     
-    x, y = prepare_dataset()
+    # x, y = prepare_dataset()
 
     np.random.seed(42); np.random.shuffle(x)
     np.random.seed(42); np.random.shuffle(y)
